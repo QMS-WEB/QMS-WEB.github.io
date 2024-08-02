6fc107803ed629bfcdb1ecfb3d99cf40d8836b0c
QMS-WEB <167092910+QMS-WEB@users.noreply.github.com>
2024-08-02 12:07:51 -0300

    Update script.js

--- talks/script.js
+++ talks/script.js
@@ -1,4 +1,4 @@
-//If you fork this, please change this database link to your own.
+// If you fork this, please change this database link to your own.
 var fb = new Firebase("https://qms-forum-default-rtdb.firebaseio.com/");
 var messages = fb.child("messages");
 var btn = $('button');
@@ -9,41 +9,40 @@ var usernameInput = $('input.username');
 var user = [];
 
 (function($) {
-	$.sanitize = function(input) {
-		var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
-					 replace(/<[\/\!]*?[^<>]*?>/gi, '').
-					 replace(/<style[^>]*?>.*?<\/style>/gi, '').
-					 replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
-	    return output;
-	};
+  $.sanitize = function(input) {
+    var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
+                  replace(/<[\/\!]*?[^<>]*?>/gi, '').
+                  replace(/<style[^>]*?>.*?<\/style>/gi, '').
+                  replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+    return output;
+  };
 })(jQuery);
 
-
 usernameInput.on('keyup', function(e) {
-	if (e.keyCode === 13 && usernameInput.val().length > 0) {
-		var getTxt = usernameInput.val();
-		user.push(getTxt);
-		usernameInput.val('');
-		$('.initModal').css('display', 'none');
-		console.log(user);
-	}
+  if (e.keyCode === 13 && usernameInput.val().length > 0) {
+    var getTxt = usernameInput.val();
+    user.push(getTxt);
+    usernameInput.val('');
+    $('.initModal').css('display', 'none');
+    wrap.css('display', 'block'); // Show the wrapper
+    input.css('display', 'block'); // Show the message input
+    console.log(user);
+  }
 });
 
-
-
 input.on('keyup', function(e) {
-	var curUsername = user.join();
-	if (e.keyCode === 13 && input.val().length > 0) {
-		var getTxt = input.val();
-		messages.push({
-			user: curUsername,
-			message: getTxt
-		});
-		input.val('');
-	}
+  var curUsername = user.join();
+  if (e.keyCode === 13 && input.val().length > 0) {
+    var getTxt = input.val();
+    messages.push({
+      user: curUsername,
+      message: getTxt
+    });
+    input.val('');
+  }
 });
 
 messages.limitToLast(100).on("child_added", function(snap) {
-	wrap.append('<li><span>' + $.sanitize(snap.val().user) + ':</span> ' + $.sanitize(snap.val().message) + '</li>');
-	window.scrollTo(0,document.body.scrollHeight);
+  wrap.append('<li><span>' + $.sanitize(snap.val().user) + ':</span> ' + $.sanitize(snap.val().message) + '</li>');
+  window.scrollTo(0,document.body.scrollHeight);
 });
