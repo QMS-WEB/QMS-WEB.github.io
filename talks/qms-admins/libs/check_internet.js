24238d8069e04c3aa5c611ae44ca23f800da5fda
QMS-WEB <167092910+QMS-WEB@users.noreply.github.com>
2024-08-16 04:54:19 -0300

    Add files via upload

---
+++ talks/qms-admins/libs/check_internet.js
@@ -0,0 +1,15 @@
+let previousInternetStatus = navigator.onLine;
+
+function checkInternetConnection() {
+  const currentInternetStatus = navigator.onLine;
+  if (previousInternetStatus !== currentInternetStatus) {
+    if (currentInternetStatus) {
+      Swal.fire('Internet connection is active', '', 'success');
+    } else {
+      Swal.fire('Internet connection is not active', '', 'error');
+    }
+    previousInternetStatus = currentInternetStatus;
+  }
+}
+
+setInterval(checkInternetConnection, 5000);
